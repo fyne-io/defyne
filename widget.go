f3be7a45ac06c6d54ae65ec1651c24b97201d2d0
Balakrishna Prasad Ganne <balkripra.1996@gmail.com>
2021-02-21 09:50:18 +0530

    table widget

--- widget.go
+++ widget.go
@@ -356,6 +356,27 @@ var widgets = map[string]widgetInfo{
 				widget.NewFormItem("Value", val)}
 		},
 	},
+	"*widget.Table": {
+		name: "Table",
+		create: func() fyne.CanvasObject {
+			return widget.NewTable(func() (int, int) { return 3, 3 }, func() fyne.CanvasObject {
+				return widget.NewLabel("Cell 000, 000")
+			}, func(id widget.TableCellID, cell fyne.CanvasObject) {
+				label := cell.(*widget.Label)
+				switch id.Col {
+				case 0:
+					label.SetText(fmt.Sprintf("%d", id.Row+1))
+				case 1:
+					label.SetText("A longer cell")
+				default:
+					label.SetText(fmt.Sprintf("Cell %d, %d", id.Row+1, id.Col+1))
+				}
+			})
+		},
+		edit: func(obj fyne.CanvasObject) []*widget.FormItem {
+			return []*widget.FormItem{}
+		},
+	},
 
 	"*fyne.Container": {
 		name: "Container",
