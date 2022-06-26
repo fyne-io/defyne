5a57bf805a8e7c40d119e08cc9a2e229a6a50771
Andy Williams <andy@fyne.io>
2022-06-26 21:04:41 +0100

    Create basic GUI to do environment check for Fyne dev

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/fyne-io/defyne
 go 1.15
 
 require (
-	fyne.io/fyne/v2 v2.2.0
+	fyne.io/fyne/v2 v2.2.1-0.20220626172616-339e704bf900
 	fyne.io/x/fyne v0.0.0-20211027195715-357fb402507f
 	github.com/fyne-io/terminal v0.0.0-20211022214227-ef292b54d29a
-	github.com/stretchr/testify v1.7.1
+	github.com/stretchr/testify v1.7.2
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad
 )
---
+++ internal/envcheck/tasks.go
@@ -0,0 +1,74 @@
+package envcheck
+
+import (
+	"errors"
+	"os"
+	"os/exec"
+	"strings"
+
+	"golang.org/x/sys/execabs"
+)
+
+type task struct {
+	name, hint string
+	test       func() (string, error)
+}
+
+var tasks = []*task{
+	{"Go compiler", "Checking Go is installed and up to date", func() (string, error) {
+		path, err := exec.LookPath("go")
+		if err != nil {
+			return "", err
+		}
+
+		cmd := execabs.Command(path, "version")
+		ret, err := cmd.Output()
+		if err != nil {
+			return "", err
+		}
+		return strings.TrimSpace(string(ret)), nil
+	}},
+	{"C compiler", "Checking C is installed for system code", func() (string, error) {
+		_, err := exec.LookPath("gcc")
+		if err == nil {
+			return "gcc found", nil
+		}
+
+		_, err = exec.LookPath("clang")
+		if err == nil {
+			return "clang found", nil
+		}
+
+		return "", errors.New("could not find gcc or clang compilers")
+	}},
+	{"Go bin PATH", "Verify that the PATH environment is set", func() (string, error) {
+		cmd := execabs.Command("go", "env", "GOPATH")
+		ret, err := cmd.Output()
+		if err != nil {
+			return "", err
+		}
+		goPath := strings.TrimSpace(string(ret))
+
+		allPath := os.Getenv("PATH")
+		if !strings.Contains(allPath, goPath) {
+			return "", errors.New("PATH missing " + goPath)
+		}
+		return "confirmed", nil
+	}},
+	//{"Dependencies installed", "Various libraries required are present", func() (string, error) {
+	//	return "", errors.New("no dependencies found")
+	//}},
+	{"Fyne helper", "Checking Fyne tool is installed for packaging", func() (string, error) {
+		path, err := exec.LookPath("fyne")
+		if err != nil {
+			return "", err
+		}
+
+		cmd := execabs.Command(path, "--version")
+		ret, err := cmd.Output()
+		if err != nil {
+			return "", err
+		}
+		return strings.TrimSpace(string(ret)), nil
+	}},
+}
